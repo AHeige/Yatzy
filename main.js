c10c9ac9fdd614b4c1f57124792b400ba9d37cac
Alexander <aheige17@gmail.com>
2019-08-19 22:37:56 +0200

    Dices working!

--- main.js
+++ main.js
@@ -1,19 +1,212 @@
 window.onload = function(){
 
-    document.getElementById("throw").addEventListener("click", throwDice)
-    x = []; //Spelare 1
-    y = []; //Spelare 2
-    z = []; //Spelare 3
+    document.getElementById("throw").addEventListener("click", throwDice);
+    document.getElementById("total-uppe").addEventListener("click", sumUp);
+    //document.getElementById("namn5").addEventListener("click", getValue);
+    x = [];//dices
+    y = [];//Chosen dices
+    z = []//Chosen dices values
+    players = [];
+    let timethrowed = 0;
+    let classlength = 0;
+    let turn = 0;
+
+    /*function getValue(){
+        let k = document.getElementById("namn1").value;
+        console.log(k);
+    }*/
+
+    document.getElementById("start").addEventListener("click", newGame);
+
+    function newGame(){
+        x = [];//dices
+        y = [];//Chosen dices
+        z = []//Chosen dices values
+        players = [];
+        clearResult();
+        let a = prompt("Hur många spelare?")
+        if (a === null){
+            return;
+        }
+        document.getElementById("grid-container").style.display = "grid";
+        document.getElementById("throw").style.display = "block";
+        let i = 1;
+        if (a > 5){
+            alert("Högst antal spelare är 5!")
+            newGame();
+            return;
+        }else
+        do {
+            let p = prompt("Namn på spelare: " + i);
+            players.push(p);
+            a--
+            i++
+        } while (a > 0);
+        let pl = players.length;
+        let b = 1;
+        let h = 0;
+        do {
+            document.getElementById("namn"+b).value = players[h];
+                pl--
+                b++
+                h++
+            } while (pl > 0)
+        whosTurn(turn);
+    }
+
+    function sumUp(){
+        let a = parseInt(document.getElementById("namn1-nr1").value) 
+                + parseInt(document.getElementById("namn1-nr2").value) 
+                + parseInt(document.getElementById("namn1-nr3").value) 
+                + parseInt(document.getElementById("namn1-nr4").value)
+                + parseInt(document.getElementById("namn1-nr5").value)
+                + parseInt(document.getElementById("namn1-nr6").value)
+        
+        let b = parseInt(document.getElementById("namn2-nr1").value) 
+                + parseInt(document.getElementById("namn2-nr2").value) 
+                + parseInt(document.getElementById("namn2-nr3").value) 
+                + parseInt(document.getElementById("namn2-nr4").value)
+                + parseInt(document.getElementById("namn2-nr5").value)
+                + parseInt(document.getElementById("namn2-nr6").value)
+
+        let c = parseInt(document.getElementById("namn3-nr1").value) 
+                + parseInt(document.getElementById("namn3-nr2").value) 
+                + parseInt(document.getElementById("namn3-nr3").value) 
+                + parseInt(document.getElementById("namn3-nr4").value)
+                + parseInt(document.getElementById("namn3-nr5").value)
+                + parseInt(document.getElementById("namn3-nr6").value)
+
+        let d = parseInt(document.getElementById("namn4-nr1").value) 
+                + parseInt(document.getElementById("namn4-nr2").value) 
+                + parseInt(document.getElementById("namn4-nr3").value) 
+                + parseInt(document.getElementById("namn4-nr4").value)
+                + parseInt(document.getElementById("namn4-nr5").value)
+                + parseInt(document.getElementById("namn4-nr6").value)
+
+        let e = parseInt(document.getElementById("namn5-nr1").value) 
+                + parseInt(document.getElementById("namn5-nr2").value) 
+                + parseInt(document.getElementById("namn5-nr3").value) 
+                + parseInt(document.getElementById("namn5-nr4").value)
+                + parseInt(document.getElementById("namn5-nr5").value)
+                + parseInt(document.getElementById("namn5-nr6").value)
+         
+        
+        document.getElementById("namn1-totuppe").value = a;
+        document.getElementById("namn2-totuppe").value = b;
+        document.getElementById("namn3-totuppe").value = c;
+        document.getElementById("namn4-totuppe").value = d;
+        document.getElementById("namn5-totuppe").value = e;
+    }
+
+    function sumTot(){
+
+    }
+
+    function clearResult(){
+        document.getElementById("result").innerHTML = "<p id='throwed'></p>";
+    }
+
+    function whosTurn(a){
+        document.getElementById("whosturn").innerHTML = "Det är spelares: " + players[a]
+    }
 
     function throwDice(){
-        let i = 0;
-        let k = document.getElementById("result");
-        do {
-            let a = Math.floor((Math.random() * 6) + 1);
-            x.push(a);
-            i++
-        } while (i < 5);
-        k.innerHTML = "Dina tärninga är: "+ x.toString()+"<br>";
+        getActive();
+        document.getElementById("next").style.display = "block";
+        document.getElementById("next").addEventListener("click", nextPlayer)
+        if (classlength < 1 && timethrowed < 3){
+            clearResult();
+            let i = 0;
+            let dicenum = 1; 
+            let k = document.getElementById("result");
+            timethrowed++
+            document.getElementById("throwed").innerHTML = "Antal kast: " +timethrowed;
+            do {
+                let dicename = "dice" + dicenum;
+                let b = k.innerHTML;
+                let a = Math.floor((Math.random() * 6) + 1);
+                k.innerHTML = b + "<div id="+dicename+">"+ a + "</div>"
+                i++
+                dicenum++
+            } while (i < 5)
+            document.getElementById("dice1").addEventListener("click", toggleActive)
+            document.getElementById("dice2").addEventListener("click", toggleActive)
+            document.getElementById("dice3").addEventListener("click", toggleActive)
+            document.getElementById("dice4").addEventListener("click", toggleActive)
+            document.getElementById("dice5").addEventListener("click", toggleActive)
+        }else
+        if (classlength > 0 && timethrowed < 3){
+            clearResult();
+            timethrowed++
+            let dicenum = 1;
+            let i = 0;
+            let k = document.getElementById("result");
+            document.getElementById("throwed").innerHTML = "Antal kast: " +timethrowed;
+            let cl = 5 - classlength;
+            do {
+                let dicename = "dice" + dicenum;
+                let b = k.innerHTML;
+                k.innerHTML = b +  "<div id="+dicename+">"+ y[i] + "</div>";
+                i++
+                dicenum++
+            } while (classlength > i)
+            do {
+                let dicename = "dice" + dicenum;
+                let a = Math.floor((Math.random() * 6) + 1);
+                let b = k.innerHTML;
+                k.innerHTML = b + "<div id="+dicename+">"+ a + "</div>"
+                cl--
+                dicenum++
+            }while (cl > 0)
+            document.getElementById("dice1").addEventListener("click", toggleActive)
+            document.getElementById("dice2").addEventListener("click", toggleActive)
+            document.getElementById("dice3").addEventListener("click", toggleActive)
+            document.getElementById("dice4").addEventListener("click", toggleActive)
+            document.getElementById("dice5").addEventListener("click", toggleActive)
+
+        }else
+        if (timethrowed = 3){
+            alert("Du har inga fler kast");
+        }
+    
+    }
+
+    function getActive(){
+        classlength = parseInt(document.getElementsByClassName("active").length);
+        let a = 0;
+        let b = document.getElementsByClassName("active")
+        if (classlength < 1){
+            return;
+        }else
+            do {
+                c = b[a].id
+                d = document.getElementById(c).innerHTML;
+                y.push(d);
+                a++
+            } while (classlength > a);
+
+    }
+
+    function toggleActive(){
+        this.classList.toggle("active")
+    }
+
+    function nextPlayer(){
+        x = [];
+        y = [];
+        z = [];
+        timethrowed = 0;
+        let b = players.length;
+        if (turn < b - 1){
+            turn++
+            whosTurn(turn);
+            clearResult();
+            console.log("if")
+        }else
+        turn = 0;
+        whosTurn(turn);
+        clearResult();
+        console.log("else")
     }
 
 }
